a76f100b6f292a86b327aee24cb92653b044b95a
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-02-05 17:19:28 +0100

    Initialized Flask app and set static folder path

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
-
+import os
 # Initialize the app
-app = Flask(__name__)
+app = Flask(__name__,template_folder=os.path.join(os.getcwd(),'views'), static_folder=os.path.join(os.getcwd(),'static'))
 
 # Import routes after app is initialized
 from app.routes import *
