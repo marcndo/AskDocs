344638c8ba35fe4857509c93c285002e809c9656
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-02-05 18:56:58 +0100

    Refactor document loading

--- app/utils.py
+++ app/utils.py
@@ -1,22 +1,21 @@
-import fitz
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
+from langchain.document_loaders import PyMuPDFLoader, CSVLoader, WebBaseLoader
 
+# Load pdf document
+def load_pdf(file_path):
+    loader = PyMuPDFLoader(file_path)
+    document = loader.load()
+    return document
 
-def parse_pdf(file_path):
-    document = fitz.open(file_path)
-    text = ""
-    for page in document:
-        text += page.get_text("text")
-    return text
+# Load csv file 
+def load_csv(file_path):
+    loader = CSVLoader(file_path)
+    document = loader.load()
+    return document
 
-def parse_csv(file_path):
-    df = pd.read_csv(file_path)
-    return df.to_string(df)
+# Load url
+def load_url(url):
+    loader = WebBaseLoader(url)
+    document = loader.load()
+    return document
 
-def parse_url(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response, "html.parser")
-    return soup.get_text()
 
